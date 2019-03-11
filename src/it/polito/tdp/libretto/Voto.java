3c3fb7728a4bee9f37be25c3cac7a06f6e28dfb0
Stefano <Stefano@172.22.57.129>
2019-03-11 13:53:25 +0100

    Aggiornamento

--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,37 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+
 public class Voto {
+	private int voto;
+	private String corso;
+	private LocalDate date;
+	public Voto(int voto, String corso, LocalDate date) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.date = date;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	
+	
 
 }
